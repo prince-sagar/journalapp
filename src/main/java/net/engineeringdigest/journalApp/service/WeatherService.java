aaca811ce812ea3fc2248bdf3e1daa142ad3be0b
Prince <sagarprince8875@gmail.com>
2026-02-22 14:46:42 +0530

    spring boot external api working optimezw

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -18,7 +18,7 @@ public class WeatherService {
     @Autowired
     private AppCache appCache;
     public WeatherResponse getWeather(String city){
-        String finalAPI = appCache.APP_CACHE.get("WEATHER_API").replace("<city>", city).replace("<apikey>", apikey);
+        String finalAPI = appCache.APP_CACHE.get(AppCache.keys.WEATHER_API.toString()).replace("<city>", city).replace("<apikey>", apikey);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
